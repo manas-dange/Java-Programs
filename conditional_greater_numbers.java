7d9a9d11ca79e864061727435c2217df7cb3fdb0
Manas Dange <notify.manas@gmail.com>
2025-01-08 18:33:32 +0530

    Add files via upload

---
+++ conditional_greater_numbers.java
@@ -0,0 +1,8 @@
+//wap to find larger of two numbers using conditional operators
+class conditional_greater_numbers{
+    public static void main(String args[]){
+        int a = 10, b = 20;
+        int c = (a>b)?a:b;
+        System.out.printf("largest "+c);
+    }
+}
