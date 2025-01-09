2bd2b7175718923bda891a18b67c565486425599
Manas Dange <notify.manas@gmail.com>
2025-01-09 21:05:53 +0530

    Add files via upload

---
+++ LabelledBreak.java
@@ -0,0 +1,12 @@
+class LabelledBreak{
+    public static void main(String args[]){
+        first : for(int i = 1; j<5; i++){
+            second : for(int j = 1; j<3; j++){
+                System.out.println("i= " +i+ ";j=" +j);
+                if(i==2){
+                    break first;
+                }
+            }
+        }
+    }
+}
